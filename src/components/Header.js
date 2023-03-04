cc0003df3807ba93b3afd2a6d9895b3bc0267faf
salimarizi <salim9asmp1pdl@gmail.com>
2023-03-04 10:16:16 +0700

    setup project and header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+
+const Header = () => {
+  const [isActiveSearch, setIsActiveSearch] = useState(false);
+
+  return (
+    <div className="w-full flex bg-primary text-white py-3 px-4">
+      <div className="search w-full flex items-center gap-2 bg-secondary rounded-md text-gray-400 py-1 px-3">
+        {!isActiveSearch && <FontAwesomeIcon icon={faSearch} />}
+
+        <input
+          name="search"
+          className="w-full border-0 bg-secondary focus:outline-0"
+          autoComplete="off"
+          placeholder="Search"
+          onFocus={() => setIsActiveSearch(true)}
+          onBlur={() => setIsActiveSearch(false)}
+        />
+
+        {isActiveSearch && <FontAwesomeIcon icon={faCircleXmark} />}
+      </div>
+      <div className="w-1/12 text-white p-2">
+        <FontAwesomeIcon icon={faHeart} size="xl" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="w-full flex flex-col">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
